705c5e4530f7a693108b676b604c3b8155ce2d0b
Detravious Brinkley <d.brinkley97@gmail.com>
2018-08-07 17:17:13 -0700

    Updated code
    Code will print the proteins that have less than 20 amino acids.

--- scripts/theRightCode.py
+++ scripts/theRightCode.py
@@ -1,7 +1,7 @@
 
 # code working now
 # next step is to find the most conserved spots
-
+# figure out the meaning of each attribute; search through actual database
 
 from Bio import SeqIO
 from Bio import SwissProt
@@ -11,6 +11,7 @@ from Bio import AlignIO
 from Bio.pairwise2 import format_alignment
 import itertools
 
+# handle = open(r"/Volumes/ENDY_LAB/code_to_test_dbs/databases/test.dat")
 
 handle = open(r"/Volumes/ENDY_LAB/code_to_test_dbs/databases/uniprot_sprot.dat")
 
@@ -21,32 +22,46 @@ print("\n")
 # match is an int that starts at 0
 # to print protein, use protein.sequence
 for match, protein in enumerate(SwissProt.parse(handle)):
-    for eachAA in protein, match:
+    g = True
+    # print("Testing protein ", match+1)
+
+    # for eachAA in protein, match:
+
 # \n--> new line/ space for readability
-        print("\n")
-        aaSet = ['Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
+    # print("\n")
+    aaSet = ['Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
+    aaSetPrintable = ''.join(aaSet);
+        # aaSetTwo = ['N','E', 'H', 'I', 'W', 'Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
 # print statement of (-----) is used to help read the code
-        print("------------------------------------------------------------")
-        print("This is my set of amino acids: " + str(aaSet))
-        print("------------------------------------------------------------")
+
 # protein.sequence[0+1] starts at the first letter in a sequence and increments to the next with +1
-        if protein.sequence[0+1] in aaSet:
+    for n in range(0,len(protein.sequence)):
+            # letter +=1
+            # print (len(protein.sequence))
+            # print(n)
+            # print("Print letter: ", match)
+        if protein.sequence[n] not in aaSet:
+            g = False
+                # print(protein.sequence[n])
+                # print(len(protein.sequence))
             # print(protein.sequence[0 + 1])
-            print(True)
-# match is an int that starts at 0; it increments everytime and counts the total number of proteins
-            print("Protein sequence in database: ", match+1)
-            db = list(protein.sequence)
-# .join turns list into strings
-            db = ''.join(db);
-            print(db)
-            print("------------------------------------------------------------")
+                # print(True)
             break
-        else:
-            print("\n")
-# to test logic/ if failed, then uncomment the lines below
-            # print(False)
-            # print("Protein sequence in database: ", match+1)
-            # db = list(protein.sequence)
-            # db = ''.join(db);
-            # print(db)
-            # print("------------------------------------------------------------")
+    if g == True and (len(protein.sequence)) >= 50:
+        print ("\n")
+        print("Reduced amino acid set = [" + aaSetPrintable + "]")
+        print ("\n")
+
+# match is an int that starts at 0; it increments everytime and counts the total number of proteins
+        print("Matching Protein")
+        print ("Index = " + str(match+1))
+        db = list(protein.sequence)
+# .join turns list into strings
+        print("Description = " + str(protein.description))
+        print("Features = " + str(protein.features[0]))
+        # print("Name = " + str(protein.name))
+        db = ''.join(db);
+        print("Sequence = " + str(db))
+        print("Length = " + str(len(protein.sequence)))
+        print ("\n")
+        print("------------------------------------------------------------")
