d82ff2bf98ac4920a6c5d77ae588aed8140e9632
Detravious Brinkley <d.brinkley97@gmail.com>
2018-08-16 15:03:40 -0700

    Update theRightCode.py

--- scripts/theRightCode.py
+++ scripts/theRightCode.py
@@ -13,10 +13,8 @@ import itertools
 
 # handle = open(r"/Volumes/ENDY_LAB/code_to_test_dbs/databases/test.dat")
 
-handle = open(r"/Volumes/ENDY_LAB/code_to_test_dbs/databases/uniprot_sprot.dat")
-
-
-print("\n")
+handle = open(
+    r"/Volumes/ENDY_LAB/code_to_test_dbs/databases/uniprot_sprot.dat")
 
 
 # match is an int that starts at 0
@@ -29,39 +27,50 @@ for match, protein in enumerate(SwissProt.parse(handle)):
 
 # \n--> new line/ space for readability
     # print("\n")
-    aaSet = ['Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
-    aaSetPrintable = ''.join(aaSet);
-        # aaSetTwo = ['N','E', 'H', 'I', 'W', 'Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
+    aaSet = ['Q', 'F', 'L', 'Y', 'K', 'D', 'M',
+             'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
+    aaSetPrintable = ''.join(aaSet)
+    # aaSetTwo = ['N','E', 'H', 'I', 'W', 'Q', 'F', 'L', 'Y', 'K', 'D', 'M', 'V', 'G', 'C', 'R', 'P', 'T', 'S', 'A']
 # print statement of (-----) is used to help read the code
 
 # protein.sequence[0+1] starts at the first letter in a sequence and increments to the next with +1
-    for n in range(0,len(protein.sequence)):
+    for n in range(0, len(protein.sequence)):
             # letter +=1
             # print (len(protein.sequence))
             # print(n)
             # print("Print letter: ", match)
         if protein.sequence[n] not in aaSet:
             g = False
-                # print(protein.sequence[n])
-                # print(len(protein.sequence))
+            # print(protein.sequence[n])
+            # print(len(protein.sequence))
             # print(protein.sequence[0 + 1])
-                # print(True)
+            # print(True)
             break
     if g == True and (len(protein.sequence)) >= 50:
-        print ("\n")
+
+        print("\n")
         print("Reduced amino acid set = [" + aaSetPrintable + "]")
-        print ("\n")
+        print("\n")
 
 # match is an int that starts at 0; it increments everytime and counts the total number of proteins
         print("Matching Protein")
-        print ("Index = " + str(match+1))
+        print("Index = " + str(match+1))
         db = list(protein.sequence)
 # .join turns list into strings
         print("Description = " + str(protein.description))
         print("Features = " + str(protein.features[0]))
-        # print("Name = " + str(protein.name))
-        db = ''.join(db);
+        db = ''.join(db)
         print("Sequence = " + str(db))
         print("Length = " + str(len(protein.sequence)))
-        print ("\n")
+        print("\n")
         print("------------------------------------------------------------")
+        # break
+        # else:
+        # print("\n")
+# to test logic/ if failed, then uncomment the lines below
+        # print(False)
+        # print("Protein sequence in database: ", match+1)
+        # db = list(protein.sequence)
+        # db = ''.join(db);
+        # print(db)
+        # print("------------------------------------------------------------")
